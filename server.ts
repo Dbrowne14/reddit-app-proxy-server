d03d767ef94c657caf0b89fa20ed8464263a5160
Dbrowne14 <davidbrowne1992@gmail.com>
2026-02-09 18:42:50 +0000

    changed to PixelArt

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT
 
 // decide on the subreddits
 const preLoadedSubReddits = [
-  "EarthPorn",
+  "PixelArt",
   "perfectloops",
   "Cinemagraphs",
   "mechanical_gifs",
